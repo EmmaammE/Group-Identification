402831983495cdcf329087808b65dbb6d013ca3d
阿拉猪 <zrcccrz@outlook.com>
2020-12-06 22:59:03 +0800

    chore: 配置eslint prettier style-lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+/* eslint-disable no-console, no-param-reassign, no-use-before-define */
 
 ReactDOM.render(
   <React.StrictMode>
