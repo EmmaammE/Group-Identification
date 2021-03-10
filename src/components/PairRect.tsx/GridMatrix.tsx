709fb9fe22c813f03d14d65484a885114d5f5b7b
wwxkxmm <1039566079@qq.com>
2021-03-10 14:28:03 +0800

    'color'

--- src/components/PairRect.tsx/GridMatrix.tsx
+++ src/components/PairRect.tsx/GridMatrix.tsx
@@ -34,8 +34,8 @@ const padding = 10;
 const colorScale = d3
   .scaleLinear<string>()
   // .domain([0, 0.5, 1])
-  .domain([0, 1])
-  .range(['#fff', '#39915f']);
+  .domain([0, 0.5, 1])
+  .range(['#ffdfb2', '#eee', '#cde8ba']);
 // 红白蓝
 // .range(['#e60d17', '#fff', '#0b69b6']);
 
@@ -427,7 +427,7 @@ const GridMatrix = ({
                 type="number"
                 min="0.0"
                 max="1.0"
-                step="0.1"
+                step="0.01"
                 value={gridSize}
                 onChange={handleGridSizeChange}
               />
@@ -437,7 +437,7 @@ const GridMatrix = ({
           <div className="input-wrapper">
             <span> Ground-truth labels: </span>
             <Gradient
-              colors={['#fff', '#9ccb3c']}
+              colors={['#ffdfb2', '#eee', '#cde8ba']}
               legends={['0%', '100%']}
               width="50px"
               height={25}
